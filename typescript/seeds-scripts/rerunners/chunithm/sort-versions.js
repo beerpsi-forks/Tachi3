0eb5dfd4b5822f98eb27f6e6b5803d83aa65fb3d
beer-psi <92439990+beer-psi@users.noreply.github.com>
2026-05-10 08:45:02 +0000

    X-VERSE-X International

--- typescript/seeds-scripts/rerunners/chunithm/sort-versions.js
+++ typescript/seeds-scripts/rerunners/chunithm/sort-versions.js
@@ -6,6 +6,8 @@ const versions = Object.keys(GetGameConfig("chunithm").versions);
 
 MutateCollection("charts-chunithm.json", (charts) => {
 	for (const chart of charts) {
+		chart.versions = [...new Set(chart.versions)];
+		
 		chart.versions.sort((a, b) => versions.indexOf(a) - versions.indexOf(b));
 	}
 
